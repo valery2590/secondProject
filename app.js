14da2990693ecc816693d7df0d721bb04eaf00d0
Valery FH <valeryfigueroah@gmail.com>
2020-12-17 12:31:01 +0100

    changes

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("thank you for logged in. Have a nice day!")
+/*
+
+
+let registrarEmail = prompt("introduce tu email");
+
+let email = registrarEmail;
+
+alert(email);
+*/
+
+function recogerDatos (){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let mensaje = "tu email es " + email + " y tu contra es " + '"' + password +'"'; 
+
+    alert("thank you for logged in. Have a nice day!")
+    console.log(mensaje)
+}
+function borrarForm (){
+    document.getElementById("form").reset();
+}
